467414b93f17e2d0a26812afc78329bb14ebea57
Emory Merryman <emory.merryman@gmail.com>
2017-12-18 01:08:57 +0000

    source .bashrc

--- root/init.user.sh
+++ root/init.user.sh
@@ -23,6 +23,7 @@ do
         ;;
     esac
 done &&
+    source ${HOME}/.bashrc &&
     (cat <<EOF
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
--- root/run.user.sh
+++ root/run.user.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-pip install awscli --upgrade --user &&
+dnf update &&
+    pip install awscli --upgrade --user &&
     echo "export PATH=\${HOME}/.local/bin:\${PATH}" >> ${HOME}/.bashrc
