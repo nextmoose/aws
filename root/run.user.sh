b23e63818b87fddebed7eac33c7d34d12f4e3b15
Emory Merryman <emory.merryman@gmail.com>
2017-12-18 01:19:46 +0000

    remove cruft that was stopping build

--- root/run.user.sh
+++ root/run.user.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-dnf update &&
-    pip install awscli --upgrade --user &&
+pip install awscli --upgrade --user &&
     echo "export PATH=\${HOME}/.local/bin:\${PATH}" >> ${HOME}/.bashrc
