780754eb005531ffdac6c99206b372e35969a617
Emory Merryman <emory.merryman@gmail.com>
2017-12-20 18:46:08 +0000

    do not use command line args

--- root/init.user.sh
+++ root/init.user.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
-while [ ${#} -gt 0 ]
-do
-    case ${1} in
-        --aws-access-key-id)
-            AWS_ACCESS_KEY_ID="${2}" &&
-                shift 2
-        ;;
-        --aws-secret-access-key)
-            AWS_SECRET_ACCESS_KEY="${2}" &&
-                shift 2
-        ;;
-        --aws-default-region)
-            AWS_DEFAULT_REGION="${2}" &&
-                shift 2
-        ;;
-        *)
-            echo Unknown Option &&
-                echo ${0} &&
-                echo ${@} &&
-                exit 64
-        ;;
-    esac
-done &&
-    source ${HOME}/.bashrc &&
+source ${HOME}/.bashrc &&
     (cat <<EOF
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
