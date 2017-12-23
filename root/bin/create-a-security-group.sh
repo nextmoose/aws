46c7b7537cf4ca1afdd98fb128eb3e187e00935e
Emory Merryman <emory.merryman@gmail.com>
2017-12-23 02:57:07 +0000

    use our own lieutenant key

--- root/bin/create-a-security-group.sh
+++ root/bin/create-a-security-group.sh
@@ -5,8 +5,9 @@ echo SOURCE: http://docs.aws.amazon.com/cli/latest/userguide/tutorial-ec2-ubuntu
     aws ec2 create-security-group --group-name devenv-sg --description "security group for development environment in EC2" | jq .GroupId &&
     aws ec2 authorize-security-group-ingress --group-name devenv-sg --protocol tcp --port 22 --cidr 0.0.0.0/0 &&
     echo Replace the CIDR range in the above with the one that you will connect from for more security. You can use the aws ec2 describe-security-groups command to admire your handiwork. &&
-    echo Next, create a key pair, which allows you to connect to the instance. &&
-    aws ec2 create-key-pair --key-name devenv-key --query 'KeyMaterial' --output text > devenv-key.pem &&
+    echo DO NOT Next, create a key pair, which allows you to connect to the instance. &&
+    echo INSTEAD Let us upload a key pair. &&
+    aws ec2 import-key-pair --key-name lieutenant-key --public-key-material "${LIEUTENANT_PUBLIC_KEY}" &&
     echo On Linux, you will also need to change the file mode so that only you have access to the key file. &&
     chmod 400 devenv-key.pem
     echo DONE
--- root/bin/delete-a-security-group.sh
+++ root/bin/delete-a-security-group.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-aws ec2 delete-security-group --group-name devenv-sg
+aws ec2 delete-security-group --group-name devenv-sg &&
+    aws ec2 delete-key-pair --key-name lieutenant-key
