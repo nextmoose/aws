f8b50f991fc019f044e46d629d5887dd15d27c6c
Emory Merryman <emory.merryman@gmail.com>
2017-12-23 11:28:06 +0000

    fixed script

--- root/bin/terminate-lieutenant-instance.sh
+++ root/bin/terminate-lieutenant-instance.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-aws ec2 describe-instances --filters rand=6920 --query 'Reservations[0].Instances[0].PublicIpAddress' &&
-aws ec2 terminate-instances --instance-ids 
+aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters rand=6920 --query "Reservations[0].Instances[0].InstanceId")
