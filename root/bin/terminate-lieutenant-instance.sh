6ffc72bf9465b7dbf8c3c170489b3339354ce639
Emory Merryman <emory.merryman@gmail.com>
2017-12-23 12:05:03 +0000

    output to text

--- root/bin/terminate-lieutenant-instance.sh
+++ root/bin/terminate-lieutenant-instance.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters Name=tag:rand,Values=6920 --query "Reservations[0].Instances[0].InstanceId")
+aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters Name=tag:rand,Values=6920 --query "Reservations[0].Instances[0].InstanceId" --output text)
