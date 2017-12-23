dd4c7e1cead0a8caed7f91a9bc942edfed0bdea3
Emory Merryman <emory.merryman@gmail.com>
2017-12-23 12:01:54 +0000

    fix scripts

--- root/bin/create-lieutenant-security-group.sh
+++ root/bin/create-lieutenant-security-group.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 aws ec2 create-security-group --group-name ${LIEUTENANT_SECURITY_GROUP} --description "security group for the lieutenant environment in EC2" &&
-    aws ec2 authorize-security-group-ingress --group-name devenv-sg --protocol tcp --port 22 --cidr 0.0.0.0/0 
+    aws ec2 authorize-security-group-ingress --group-name ${LIEUTENANT_SECURITY_GROUP} --protocol tcp --port 22 --cidr 0.0.0.0/0 
