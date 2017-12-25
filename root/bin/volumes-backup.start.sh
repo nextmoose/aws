a0caab98f890be3ced705ed3df211ec20b453ea9
Emory Merryman <emory.merryman@gmail.com>
2017-12-25 23:56:04 +0000

    fuck it

--- root/bin/volumes-backup.start.sh
+++ root/bin/volumes-backup.start.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 
-aws ec2 create-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --description "security group for the volumes backup environment in EC2" &&
+aws \
+    ec2 \
+    wait \
+    instance-running \
+    --instance-ids \
+        $(aws \
+            ec2 \
+            run-instances \
+            --image-id ami-c998b6b2 \
+            --security-group-ids $(aws ec2 create-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --description "security group for the volumes backup environment in EC2" --query "GroupId" --output text) \
+            --count 1 \
+            --instance-type t2.micro \
+            --key-name $(aws ec2 import-key-pair --key-name ${VOLUMES_BACKUP_KEYPAIR} --public-key-material "${VOLUMES_BACKUP_PUBLIC_KEY}" --query "KeyName" --output text) \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=rand,Value=29105}]" \
+            --query "Instances[0].InstanceId" \
+            --output text
+        ) &&
     aws ec2 authorize-security-group-ingress --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --protocol tcp --port 22 --cidr 0.0.0.0/0 &&
-    aws ec2 import-key-pair --key-name ${VOLUMES_BACKUP_KEYPAIR} --public-key-material "${VOLUMES_BACKUP_PUBLIC_KEY}" &&
-    aws ec2 wait instance-started $(aws \
-        ec2 \
-        run-instances \
-        --image-id ami-c998b6b2 \
-        --security-groups ${VOLUMES_BACKUP_SECURITY_GROUP} \
-        --count 1 \
-        --instance-type t2.micro \
-        --key-name ${VOLUMES_BACKUP_KEYPAIR} \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=rand,Value=29105}]"
-        --query "Reservations[0].Instances[0].InstanceId") &&
     aws \
         ec2 \
-        create-volume \
-        --availability-zone $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 --query "Reservations[0].Instances[0].Placement.AvailabilityZone" --output text) \
-        --no-encrypted \
-        --size 8 \
-        --tag-specifications "ResourceType=volume,Tags=[{Key=rand,Value=29105}]" &&
+        wait \
+        volume-available \
+        --volume-ids $(
+            aws \
+                ec2 \
+                create-volume \
+                --availability-zone $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 --query "Reservations[0].Instances[0].Placement.AvailabilityZone" --output text) \
+                --no-encrypted \
+                --size 8 \
+                --tag-specifications "ResourceType=volume,Tags=[{Key=rand,Value=29105}]" \
+                --query "VolumeId" \
+                --output text
+            ) &&
     aws \
         ec2 \
         attach-volume \
--- root/bin/volumes-backup.stop.sh
+++ root/bin/volumes-backup.stop.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-INSTANCE_ID=$(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].InstanceId" --output text) &&
-    aws ec2 terminate-instances --instance-ids ${INSTANCE_ID} &&
-    aws ec2 wait instance-terminated --instance-ids ${INSTANCE_ID} &&
+aws ec2 wait instance-terminated --instance-ids $(aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId" --output text)) &&
     aws ec2 delete-key-pair --key-name ${VOLUMES_BACKUP_KEYPAIR} &&
     aws ec2 delete-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} &&
     aws ec2 delete-volume --volume-id $(aws ec2 describe-volumes --filters Name=tag:rand,Values=29105 --query "Volumes[0].VolumeId" --output text) &&
