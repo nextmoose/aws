300c752bc29c16357d5795a982cdb75de6504433
Emory Merryman <emory.merryman@gmail.com>
2017-12-25 19:06:15 +0000

    new wave

--- root/bin/volumes-backup.start.sh
+++ root/bin/volumes-backup.start.sh
@@ -3,7 +3,7 @@
 aws ec2 create-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --description "security group for the volumes backup environment in EC2" &&
     aws ec2 authorize-security-group-ingress --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --protocol tcp --port 22 --cidr 0.0.0.0/0 &&
     aws ec2 import-key-pair --key-name ${VOLUMES_BACKUP_KEYPAIR} --public-key-material "${VOLUMES_BACKUP_PUBLIC_KEY}" &&
-    aws \
+    aws ec2 wait instance-started $(aws \
         ec2 \
         run-instances \
         --image-id ami-c998b6b2 \
@@ -11,7 +11,8 @@ aws ec2 create-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --de
         --count 1 \
         --instance-type t2.micro \
         --key-name ${VOLUMES_BACKUP_KEYPAIR} \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=rand,Value=29105}]" &&
+        --tag-specifications "ResourceType=instance,Tags=[{Key=rand,Value=29105}]"
+        --query "Reservations[0].Instances[0].InstanceId") &&
     aws \
         ec2 \
         create-volume \
@@ -19,13 +20,6 @@ aws ec2 create-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --de
         --no-encrypted \
         --size 8 \
         --tag-specifications "ResourceType=volume,Tags=[{Key=rand,Value=29105}]" &&
-    while [ $(aws ec2 describe-instance-status --instance-id $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].InstanceId" --output text) --query "InstanceStatuses[0].InstanceState.Name" --output text) != "running" ]
-    do
-        echo waiting for instance &&
-            aws ec2 describe-instance-status --instance-id $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].InstanceId" --output text) --query "InstanceStatuses[0]" &&
-            sleep 10s
-    done &&
-    sleep 1m &&
     aws \
         ec2 \
         attach-volume \
