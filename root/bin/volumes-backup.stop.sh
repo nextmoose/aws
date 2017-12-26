aa2ac15fb0bd913968a32d4d957716eb4f4ba885
Emory Merryman <emory.merryman@gmail.com>
2017-12-26 02:23:16 +0000

    common filtering

--- root/bin/volumes-backup.stop.sh
+++ root/bin/volumes-backup.stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-aws ec2 wait instance-terminated --instance-ids $(aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId" --output text)) &&
+aws ec2 wait instance-terminated --instance-ids $(aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId" --output text) --query "TerminatingInstances[*].InstanceId" --output text) &&
     aws ec2 delete-key-pair --key-name ${VOLUMES_BACKUP_KEYPAIR} &&
     aws ec2 delete-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} &&
     for VOLUME in $(aws ec2 describe-volumes --filters Name=tag:rand,Values=29105 --query "Volumes[*].VolumeId" --output text)
