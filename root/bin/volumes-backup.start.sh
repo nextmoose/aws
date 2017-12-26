b5988c7514d4a163d900e83fe08ae222a8ae2d52
Emory Merryman <emory.merryman@gmail.com>
2017-12-26 00:41:23 +0000

    wtf

--- root/bin/volumes-backup.start.sh
+++ root/bin/volumes-backup.start.sh
@@ -39,7 +39,7 @@ aws \
         --volume-id $(aws ec2 describe-volumes --filters Name=tag:rand,Values=29105 --query "Volumes[0].VolumeId" --output text) \
         --instance-id $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].InstanceId" --output text) &&
     TEMP1=$(mktemp) &&
-    TEMP2=$(mktemp) &&*
+    TEMP2=$(mktemp) &&
     echo "${VOLUMES_BACKUP_PRIVATE_KEY}" > ${TEMP1} &&
     ssh-keyscan $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].PublicIpAddress" --output text) > ${TEMP2} &&
     ssh -i ${TEMP1} -l ec2-user -o UserKnownHostsFile=${TEMP2} $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].PublicIpAddress" --output text) "sudo mkfs -t ext4 /dev/xvdh && sudo mount /dev/xvdh /data" &&
