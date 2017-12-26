b8185e5a260947826e3e525fd1dba160fbc30634
Emory Merryman <emory.merryman@gmail.com>
2017-12-26 00:32:24 +0000

    starting

--- root/bin/volumes-backup.start.sh
+++ root/bin/volumes-backup.start.sh
@@ -15,8 +15,7 @@ aws \
             --key-name $(aws ec2 import-key-pair --key-name ${VOLUMES_BACKUP_KEYPAIR} --public-key-material "${VOLUMES_BACKUP_PUBLIC_KEY}" --query "KeyName" --output text) \
             --tag-specifications "ResourceType=instance,Tags=[{Key=rand,Value=29105}]" \
             --query "Instances[0].InstanceId" \
-            --output text
-        ) &&
+            --output text) &&
     aws ec2 authorize-security-group-ingress --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --protocol tcp --port 22 --cidr 0.0.0.0/0 &&
     aws \
         ec2 \
@@ -39,6 +38,9 @@ aws \
         --device /dev/sdh \
         --volume-id $(aws ec2 describe-volumes --filters Name=tag:rand,Values=29105 --query "Volumes[0].VolumeId" --output text) \
         --instance-id $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].InstanceId" --output text) &&
-    echo "${VOLUMES_BACKUP_PRIVATE_KEY}" > /home/user/.ssh/volumes_backup_id_rsa &&
-    sed -i "s#\${VOLUMES_BACKUP}#$(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 --query "Reservations[0].Instances[0].PublicIpAddress" --output text)#" /home/user/.ssh/config &&
-    ssh volumes_backup "sudo mkfs -t ext4 /dev/xvdh && sudo mount /dev/xvdh /data"
+    TEMP1=$(mktemp) &&
+    TEMP2=$(mktemp) &&*
+    echo "${VOLUMES_BACKUP_PRIVATE_KEY}" > ${TEMP1} &&
+    ssh-keyscan $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].PublicIpAddress" --output text) > ${TEMP2} &&
+    ssh -i ${TEMP1} -l ec2-user -o UserKnownHostsFile=${TEMP2} $(aws ec2 describe-instances --filters Name=tag:rand,Values=29105 Name=instance-state-name,Values=running --query "Reservations[0].Instances[0].PublicIpAddress" --output text) "sudo mkfs -t ext4 /dev/xvdh && sudo mount /dev/xvdh /data" &&
+    rm -f ${TEMP1} ${TEMP2}
